9e263e14bb27b8431624c39d6198001fec0a42d7
Yusuf Hasan Miyan <yusuf.hasanmiyan@acquia.com>
2015-12-21 15:33:41 +1100

    Fixes for the 22 Dec 2015 release
     - Modified the no-results message to add <q> tags
    
     - Added external link icon
    
     - Fixed footer bottom menu responsiveness
    
     - Removed leading space from comment

--- crimtrac_theme/template.php
+++ crimtrac_theme/template.php
@@ -135,3 +135,63 @@ function crimtrac_preprocess_block(&$variables, $hook) {
   // $variables['classes_array'][] = 'count-' . $variables['block_id'];
 }
 // */
+
+/**
+ * Implements template_preprocess_search_api_page_results to
+ * wrap term by <q> tag
+ *
+ * @see template_preprocess_search_api_page_results
+ */
+function crimtrac_preprocess_search_api_page_results(&$variables) {
+  // copied from template_preprocess_search_api_page_results
+  // Make the help text depend on the parse mode used.
+  switch ($variables['page']->options['mode']) {
+    case 'direct':
+      if ($variables['index']->server()->class == 'search_api_solr') {
+        $variables['no_results_help'] = t('<ul>
+<li>Check if your spelling is correct.</li>
+<li>Remove quotes around phrases to search for each word individually. <q>bike shed</q> will often show more results than <q>&quot;bike shed&quot;</q>.</li>
+<li>Consider loosening your query with <q>OR</q>. <q>bike OR shed</q> will often show more results than <q>bike shed</q>.</li>
+</ul>');
+      }
+      else {
+        $variables['no_results_help'] = t('<ul>
+<li>Check if your spelling is correct.</li>
+<li>Use fewer keywords to increase the number of results.</li>
+</ul>');
+      }
+      break;
+    case 'single':
+      $variables['no_results_help'] = t('<ul>
+<li>Check if your spelling is correct.</li>
+<li>Use fewer keywords to increase the number of results.</li>
+</ul>');
+      break;
+    case 'terms':
+      $variables['no_results_help'] = t('<ul>
+<li>Check if your spelling is correct.</li>
+<li>Remove quotes around phrases to search for each word individually. <q>bike shed</q> will often show more results than <q>&quot;bike shed&quot;</q>.</li>
+<li>Use fewer keywords to increase the number of results.</li>
+</ul>');
+      break;
+  }
+}
+
+/*
+ * Copied from search_help except we're using
+ * only a very specific part of it, to wrap
+ * quotes with the <q> element. This function
+ * is used in the overridden search-results.tpl.php
+ *
+ * @see search_help
+ */
+function crimtrac_search_help($path, $arg) {
+  switch ($path) {
+    case 'search#noresults':
+      return t('<ul>
+<li>Check if your spelling is correct.</li>
+<li>Remove quotes around phrases to search for each word individually. <q>bike shed</q> will often show more results than <q>&quot;bike shed&quot;</q>.</li>
+<li>Consider loosening your query with <q>OR</q>. <q>bike OR shed</q> will often show more results than <q>bike shed</q>.</li>
+</ul>');
+  }
+}
